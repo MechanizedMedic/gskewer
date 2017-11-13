b1d58068a7dbe455b1d4bde823181e123eb9fdff
MechanizedMedic <mcsatterlee@gmail.com>
2017-11-13 14:03:04 -0800

    added CLI capability
    coded CLI functionality but it still needs testing. The readme will also
    need updating to ensure clear instructions

--- skew.py
+++ skew.py
@@ -1,19 +1,70 @@
 #!/usr/bin/env python3
 import re
 import os
+import argparse
 
-# Define the tangent angles that will be used later
-# This is the axis error from 90 degrees (measured in mm)
-xyerr = 0.7
-yzerr = 0.25
-zxerr = 0.1
+parser = argparse.ArgumentParser(description='gskewer is a tool to skew the coordinates in a G-code file.')                                               
 
-# length of the side where the error was measured (measured in mm)
-xylen = 50
-yzlen = 50
-zxlen = 50
+xygroup = parser.add_mutually_exclusive_group(required=True)
+xygroup.add_argument('--xyerr', type=float, help='Error in the X-axis for the XY pair in mm.')
+xygroup.add_argument('--xytan', type=float, help='The error in the XY pair as a tangent. (xyerr/xylen)')
 
-filename = 'H_Huntsman_Z_Mount.gcode'
+
+yzgroup = parser.add_mutually_exclusive_group(required=True)
+yzgroup.add_argument('--yzerr', type=float, help='Error in the Y-axis for the YZ pair in mm.')
+yzgroup.add_argument('--yztan', type=float, help='The error in the YZ pair as a tangent. (yzerr/yzlen)')
+
+
+zxgroup = parser.add_mutually_exclusive_group(required=True)
+zxgroup.add_argument('--zxerr', type=float, help='Error in the Z-axis for the ZX pair in mm.')
+zxgroup.add_argument('--zxtan', type=float, help='The error in the ZX pair as a tangent. (zxerr/zxlen)')
+
+
+parser.add_argument('--xylen', default=100, type=int, help='Length of the side where the XY error measurement was taken. This is optional, default value is 100mm')
+parser.add_argument('--yzlen', default=100, type=int, help='Length of the side where the YZ error measurement was taken. This is optional, default value is 100mm')
+parser.add_argument('--zxlen', default=100, type=int, help='Length of the side where the ZX error measurement was taken. This is optional, default value is 100mm')
+
+# input file setup. name of input file is not in "args.file"
+parser.add_argument("file", type=str)
+
+#parser.add_argument('-v', action='count', help='Increases terminal output verbosity. More V's will increase verbosity' )
+
+args = parser.parse_args()
+
+if args.xytan:
+	xytan = args.xytan
+elif args.xyerr:
+	xytan = args.xyerr/args.xylen
+else:
+	xytan = 0.0
+
+if not xytan == 0:
+	print('The XY error is set to', xytan,'degrees')
+	
+if args.yztan:
+	yztan = args.yztan
+elif args.yzerr:
+	yztan = args.yzerr/args.yzlen
+else:
+	yztan = 0.0
+
+if not yztan == 0:
+	print('The YZ error is set to', yztan,'degrees')
+
+if args.zxtan:
+	zxtan = args.zxtan
+elif args.zxerr:
+	zxtan = args.zxerr/args.zxlen
+else:
+	zxtan = 0.0
+
+if not zxtan == 0:
+	print('The ZX error is set to', zxtan,'degrees')
+	
+if xytan == 0.0 and yztan == 0.0 and zxtan == 0.0: 
+	print('No skew parameters provided. Nothing will be done.')
+	
+filename = args.file
 
 outname = 'skewed-'+filename
 
@@ -33,25 +84,25 @@ with open(filename,'r') as infile:
         if gmatch:
             print('line was a G0/G1 command!')
 
-            # load the incoming X coordinate into a variable
+            # load the incoming X coordinate into a variable. Previous value will be used if new value is not found.
             xsrch = re.search(r'[xX]\d*\.*\d*',line,re.I)
             if xsrch: # if an X value is found
-                xin = float(re.sub(r'[xX]','',xsrch.group()))
+                xin = float(re.sub(r'[xX]','',xsrch.group())) # Strip the letter from the coordinate.
 
-            # load the incoming Y coordinate into a variable
+            # load the incoming Y coordinate into a variable. Previous value will be used if new value is not found.
             ysrch = re.search(r'[yY]\d*\.*\d*', line, re.I)
             if ysrch:
-                yin = float(re.sub(r'[yY]','',ysrch.group()))
+                yin = float(re.sub(r'[yY]','',ysrch.group())) # Strip the letter from the coordinate.
 
-            # load the incoming Z coordinate into a variable
+            # load the incoming Z coordinate into a variable. Previous value will be used if new value is not found.
             zsrch = re.search(r'[zZ]\d*\.*\d*', line, re.I)
             if zsrch:
-                zin = float(re.sub(r'[zZ]','',zsrch.group()))
+                zin = float(re.sub(r'[zZ]','',zsrch.group())) # Strip the letter from the coordinate.
 
             # calculate the corrected/skewed XYZ coordinates
-            xout = round(xin-yin*(xyerr/xylen),3)
-            yout = round(yin-zin*(yzerr/yzlen),3)
-            zout = round(zin-xin*(zxerr/zxlen),3)
+            xout = round(xin-yin*xytan,3)
+            yout = round(yin-zin*yztan,3)
+            zout = round(zin-xin*zxtan,3)
 
             lineout = line
             print('old line:', lineout)
